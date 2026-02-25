a3433808a7ab65ec72c6184fb716c5381fd18f5f
Osman Kahraman <o_kahraman@outlook.com>
2026-02-25 16:32:56 -0500

    Initial Commit

---
+++ GitSwipe/GitSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSwipeApp.swift
+//  GitSwipe
+//
+//  Created by Osman Kahraman on 2026-02-25.
+//
+
+import SwiftUI
+
+@main
+struct GitSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
